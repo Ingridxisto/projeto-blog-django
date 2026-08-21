2b34196b7da49a6878cb155e90b88d01638e7abc
Ingrid Xisto <ingridxisto@gmail.com>
2026-08-21 10:31:56 -0300

    Migrando a view search para Class Base View (CBV) ListView em views.py do Django

--- djangoapp/blog/urls.py
+++ djangoapp/blog/urls.py
@@ -1,5 +1,11 @@
-from blog.views import (post, page, CreatedByListView, category, tag, search,
-                        PostListView)
+from blog.views import (
+    PostListView,
+    CreatedByListView, CategoryListView,
+    TagListView,
+    SearchListView,
+    page,
+    post,
+    )
 from django.urls import path
 
 app_name = 'blog'
@@ -9,9 +15,9 @@ urlpatterns = [
     path('post/<slug:slug>/', post, name='post'),
     path('page/<slug:slug>/', page, name='page'),
     path(
-        'created_by/<int:author_pk>/',
+        'created_by/<int:author_pk>/,',
         CreatedByListView.as_view(), name='created_by'),
-    path('category/<slug:slug>/', category, name='category'),
-    path('tag/<slug:slug>/', tag, name='tag'),
-    path('search/', search, name='search'),
+    path('category/<slug:slug>/', CategoryListView.as_view(), name='category'),
+    path('tag/<slug:slug>/', TagListView.as_view(), name='tag'),
+    path('search/', SearchListView.as_view(), name='search'),
 ]
